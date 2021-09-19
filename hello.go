a9722ba4a47a258d2b6ec1b884e7bd7717b623e0
Akshay448 <akshaykapoor448@gmail.com>
2021-09-20 00:09:21 +0530

    added package morestrings and the corresponding testcase

--- hello.go
+++ hello.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 import "rsc.io/quote"
+import "example/hello/morestrings"
 
 func main() {
 	fmt.Println("Hello duniya")
 	fmt.Println(quote.Hello())
+	fmt.Println(morestrings.ReverseRunes("!oG , olleH"))
 }
